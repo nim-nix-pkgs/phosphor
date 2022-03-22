8f62253089e3b692a58c95cbd062f5286ba377b7
hugosenari <hugosenari@gmail.com>
2022-03-22 00:36:28 -0300

    chore: re-index ../nimpkgs/p/phosphor

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,6 @@
 {
-  description = ''eaiser use of OpenGL and GLSL shaders'';
+  description = ''Phosphor is a library that uses light metaprogramming to make
+                 writing openGL code eaiser'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,13 +10,14 @@
   
   inputs.src-Phosphor-master.flake = false;
   inputs.src-Phosphor-master.owner = "barcharcraz";
-  inputs.src-Phosphor-master.ref   = "refs/heads/master";
+  inputs.src-Phosphor-master.ref   = "master";
   inputs.src-Phosphor-master.repo  = "Phosphor";
   inputs.src-Phosphor-master.type  = "github";
   
   inputs."opengl".owner = "nim-nix-pkgs";
   inputs."opengl".ref   = "master";
   inputs."opengl".repo  = "opengl";
+  inputs."opengl".dir   = "1_2_6";
   inputs."opengl".type  = "github";
   inputs."opengl".inputs.nixpkgs.follows = "nixpkgs";
   inputs."opengl".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
