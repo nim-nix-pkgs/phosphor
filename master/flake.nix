0f24a00862c4e556ab4537f1cdcea6d44e9744ea
hugosenari <hugosenari@gmail.com>
2022-03-27 21:37:18 -0300

    chore: re index phosphor

--- master/flake.nix
+++ master/flake.nix
@@ -12,6 +12,7 @@
   inputs.src-Phosphor-master.ref   = "refs/heads/master";
   inputs.src-Phosphor-master.owner = "barcharcraz";
   inputs.src-Phosphor-master.repo  = "Phosphor";
+  inputs.src-Phosphor-master.dir   = "";
   inputs.src-Phosphor-master.type  = "github";
   
   inputs."opengl".owner = "nim-nix-pkgs";
